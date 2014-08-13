e16f08c2090a6cf968ea9ce48c9a17f32cee98da
Rani Itani <itani.rani@gmail.com>
2014-08-13 16:35:38 -0400

    step 3, bro

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,22 @@
+#step 1
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list.each {|x| puts " *" + x}
 
+#step 2
+grocery_list << "Rice"
+puts grocery_list
+grocery_list.each {|x| puts " *" + x}
 
-print grocery_list
+def listdisplay(argument)
+	argument.each {|x| puts " *" + x}
+end
+
+listdisplay(grocery_list)
+
+#step 3
+if  grocery_list.include?("bananas")  
+	puts "You don't need bananas"
+	else 
+		puts "You need to pick up bananas"
+
+end
